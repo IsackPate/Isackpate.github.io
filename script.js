f9bbb65c9514d3b37badb77ebe4cd539f2782db2
isackpate <isackpate@gmail.com>
2025-05-14 14:02:45 +0300

    Add files via upload
    Upload website

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 
-<script>
-document.getElementById('product-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-  var productName = document.getElementById('product-name').value;
-  var productPrice = document.getElementById('product-price').value;
-  var productDescription = document.getElementById('product-description').value;
-  var productImage = document.getElementById('product-image').files[0];
-  
-  // Firebase function to save the product (pseudo-code)
-  var storageRef = firebase.storage().ref('products/' + productImage.name);
-  storageRef.put(productImage).then(function(snapshot) {
-    snapshot.ref.getDownloadURL().then(function(url) {
-      var productRef = firebase.database().ref('products');
-      productRef.push({
-        name: productName,
-        price: productPrice,
-        description: productDescription,
-        imageUrl: url
-      });
-      alert('Product added successfully!');
-    });
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Isack Pate site loaded!");
 });
-</script>
